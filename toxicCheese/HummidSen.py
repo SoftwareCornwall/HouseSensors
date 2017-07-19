6f8c0d63befa2d8a7c7e6b6aafb6754e1e6fe17c
Mark <m1pearson@me.com>
2017-07-19 14:12:32 +0100

    Change so stores temp and humid in two different files and get rid of description for numbers

--- toxicCheese/HummidSen.py
+++ toxicCheese/HummidSen.py
@@ -6,31 +6,54 @@ import os
 
 
 sense = SenseHat()
-fileLocation = "/var/www/html/HumTemp.txt"
-Time = ""
-sMessage = ""
 
-if os.path.isfile(fileLocation) == False:
-    file = open(fileLocation,"w")
-    file.close()
+HummidLocation = "/var/www/html/HummidtyData.txt"
+TemperatureLocation = "/var/www/html/TemperatureData.txt"
+
+sMessageHummid = ""
+sMessageTemp = ""
+
+if os.path.isfile(HummidLocation) == False:
+    Hummidfile = open(HummidLocation,"w")
+    Hummidfile.close()
+
+if os.path.isfile(TemperatureLocation) == False:
+    Temperaturefile = open(TemperatureLocation,"w")
+    Temperaturefile.close()
 
 while True:
     
-    file = open(fileLocation,"a")
+    Hummidfile = open(HummidLocation,"a")
+    Temperaturefile = open(TemperatureLocation,"a")
+    
+
+
+    sMessageHummid = "\n"
+    sMessageTemp ="\n"
+
+    
+    #Hummidity
     humidity = sense.get_humidity()
+    sMessageHummid += str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    sMessageHummid += " "
+    sMessageHummid += str(round(sense.humidity, 2))
+
+    
+    
+    #Temperature
     temp = sense.get_temperature()
+    sMessageTemp += str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    sMessageTemp += " "        
+    sMessageTemp += str(round(sense.temperature, 2))
 
-    sMessage = "\n"
-    sMessage += "TimeStamp: "
-    sMessage += str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    sMessage += "   Humidity: "
-    sMessage += str(round(sense.humidity, 2))
-    sMessage += "   Temp: "
-    sMessage += str(round(sense.temperature, 2))
-
-    file.write(sMessage)
-    print (sMessage)
-    file.close()
+    Hummidfile.write(sMessageHummid)
+    Temperaturefile.write(sMessageTemp)
+    
+    print (sMessageHummid)
+    print (sMessageTemp)
+    
+    Hummidfile.close()
+    Temperaturefile.close()
     
     sleep(3)
     
