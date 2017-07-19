5e9cb115078679090ee3c75845ef47d64f091334
Mark <m1pearson@me.com>
2017-07-19 16:01:14 +0100

    Made changes so that there are links on the web page to the raw data for Humidity & Temperature

--- toxicCheese/BuildPage.py
+++ toxicCheese/BuildPage.py
@@ -28,10 +28,12 @@ while True:
     sPage += " Gateway Business Centre, Redruth TR15 3RQ" #1st header
     sPage += "</h1>"
     sPage += paragraph("<a href='https://www.google.com/maps/place/Bluefruit+Software,+Gateway+Business+Centre,+Barncoose+Gateway+Park,+Illogan+Highway,+Redruth+TR15+3RQ,+UK/@50.228803,-5.24783,17z/data=!4m2!3m1!1s0x486b1f5c8ba8f665:0x5d7c3f05d682b4c3'>Location</a>")
-    sPage += paragraph("Day: " + str(HummidList[-1][0:10]))
+    sPage += paragraph("Date: " + str(HummidList[-1][0:10]))
     sPage += paragraph("Humidity: " + str(HummidList[-1][20:25]) + "%,---" + "(Last Update: " + str(HummidList[-1][11:19]) + ")")
     sPage += paragraph("Temperature: " + str(TempList[-1][20:25]) + "&deg;C,---" + "(Last Update: " + str(TempList[-1][11:19]) + ")")
-
+    sPage += paragraph("<a href='http://10.160.50.195/HummidtyData.txt'> Raw Humidity Data</a>" )
+    sPage += paragraph("<a href='http://10.160.50.195/TemperatureData.txt'> Raw Temperature Data</a>" )
+    
     sPage += "</body> </html>"
 
     file = open("/var/www/html/SensorData.html","w")
