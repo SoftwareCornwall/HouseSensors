fd54619226a83dad3876604ec7365e36f7e2ed37
vikke1234 <viktor.horsmanheimo@gmail.com>
2017-07-19 12:06:17 +0100

    bug fixes

--- toxicCheese/main.c
+++ toxicCheese/main.c
@@ -15,29 +15,34 @@ int main(int argc, char *argv[]){
 	int fd = 0;
 	fd = open(filename, O_RDONLY);
 	if (fd < 0){
-		printf("could not open: %s", filename);
+		printf("could not open: %s\n", filename);
 		exit(1);	
 	}
+
 	if(ioctl(fd, I2C_SLAVE, READ)){
-		printf("error setting up i2c port");
+		printf("error setting up i2c port\n");
 		exit(1);	
 	}
+
 	size_t bytesToRead = 1;
 	while(1){
-	size_t bytesRead = read(fd, buffer, bytesToRead);
-	int x = 0;
-	
-	if(bytesRead != bytesToRead){
-		printf("error reading from: %s", filename);
+
+		buffer[0] = 0x28;
+		write(fd, buffer, 1);
+		size_t bytesRead = read(fd, buffer, bytesToRead);
+		int x = 0;
+
+		if(bytesRead != bytesToRead){
+			printf("error reading from: %s\n", filename);
+			close(fd);
+			exit(1);
+		}
+		printf("humidity: %d, %d %d\n", buffer[0], buffer[1], buffer[2]);
+		sleep(3);
+		if(x == 10)
+			break;
+		x++;
 		close(fd);
-		exit(1);
-	}
-	printf("humidity: %d, %d %d\n", buffer[0], buffer[1], buffer[2]);
-	sleep(3);
-	if(x == 10)
-		break;
-	x++;
-	close(fd);
-	return 0;
+		return 0;
 	}
 }
