14db8ff6ac4dfbe4760ed680e11a7f1ae2e4bd2e
vikke1234 <viktor.horsmanheimo@gmail.com>
2017-07-20 09:11:43 +0100

    bug fixes

--- toxicCheese/main.c
+++ toxicCheese/main.c
@@ -76,15 +76,14 @@ int main(int argc, char *argv[]){
 
 		rH0 = shift(fd, buffer, H0_rH_x2);
 		rH1 = shift(fd, buffer, H1_rH_x2);
+
 		humidity = shift(fd, buffer, HUMIDITY_OUT_H);
+
 		printf("humidity: %x\n", humidity );
 		printf("rH0: %x\nrH1: %x\nH0: %x\nH1: %x\n", rH0, 
-			rH1, H0_T0_OUT, H1_T0_OUT);
+			rH1, H0_T0_OUT,(int) H1_T0_OUT);
 		printf("sizeof var %lu\n",sizeof(H1_T0_OUT));
 		sleep(3);
-		if(x == 10)
-			break;
-		x++;
 	}
 	close(fd);
 	return 0;
