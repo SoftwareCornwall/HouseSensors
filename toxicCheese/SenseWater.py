188f2135bd1344cbeb3fab0a7e897af90249c082
LukeBowden <lukebowden1@gmail.com>
2017-07-21 17:11:37 +0100

    Sense water object

--- toxicCheese/SenseWater.py
+++ toxicCheese/SenseWater.py
@@ -3,17 +3,14 @@ import os
 import time
 import RPi.GPIO as GPIO
 import datetime
+
 now = datetime.datetime.now()
 GPIO.setwarnings(False)
+# tell the GPIO module that we want to use 
+# the chip's pin numbering scheme
+GPIO.setmode(GPIO.BCM)
 
-
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc: # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else: raise
+print ("Hello")
 
 #relation ship between raw number and liters
 timesFactorForMin = 230
@@ -24,18 +21,20 @@ HouseNumber = 21
 Senser = "Rate Of Liquid Flow"
 
 WaterDataLocation = "/home/pi/Desktop/HouseSensors/toxicCheese/House" + str(HouseNumber) + "/"+ str(HouseNumber) +"WaterData_" + (now.strftime("%Y_%m_%d")) + ".csv"
-print (WaterDataLocation)
-ivelocity = 0
+WaterDataDirectory = "/home/pi/Desktop/HouseSensors/toxicCheese/House" + str(HouseNumber)
+ 
+if not os.path.exists(WaterDataDirectory):
+    os.makedirs(WaterDataDirectory)
+
+
 icounter = 0
 start = 0
-end = 0
+#end = 0
 
 sMessageWater = ""
 
-if os.path.isfile(WaterDataLocation) == False:
-    mkdir_p(os.path.dirname(WaterDataLocation))
-    Waterfile = open(WaterDataLocation,"w")
-    Waterfile.close()
+
+
 
  
 # handle the button event
@@ -48,17 +47,8 @@ def buttonEventHandler (pin):
     icounter += 1
 
 
-
-
-# main function
-def main():
-    global icounter
-    global start
-    # tell the GPIO module that we want to use 
-    # the chip's pin numbering scheme
-    GPIO.setmode(GPIO.BCM)
-
-    # setup pin 23 as an input
+def StartUp():
+    #setup pin 23 as an input
     # and set up pins 24 and 25 as outputs
     GPIO.setup(23,GPIO.IN)
     GPIO.setup(24,GPIO.OUT)
@@ -69,29 +59,30 @@ def main():
     # the buttonEventHandler function
     GPIO.add_event_detect(23,GPIO.FALLING)
     GPIO.add_event_callback(23,buttonEventHandler) 
+    
 
 
-
-
+# main function
+def main():
+    global icounter
+    global start
+    
     while True:
+        #time.sleep(1)
         TimeDiff = time.time() - start
         if (TimeDiff < 1.01) and (TimeDiff > 0.99):
-            sWaterMessage = str(now.strftime("%H:%M:%S")) + "," + str(icounter/timesFactorForMin)
-            print (sWaterMessage)
-            GPIO.cleanup()
+            now = datetime.datetime.now()
+            sWaterMessage = str(now.strftime("%H:%M:%S")) + "," + str(round(icounter/timesFactorForMin, 4)) + "\n"
+            #print (sWaterMessage)
             Waterfile = open(WaterDataLocation,"a")
             Waterfile.write(sWaterMessage)
-            return(sWaterMessage)
-            icounter = 0
-            start = 0
-            
-            
+            Waterfile.close()
+            return (sWaterMessage)
 
 
-    GPIO.cleanup()
-
-
-
-icounter = 0
-start = 0
-print (main())
+StartUp()
+while True:
+    icounter = 0
+    start = 0
+    print (main())
+    time.sleep(1)
