a4e07e5a0fd5bd741b0c1336c66e265c0f89ce7d
James Hui <huitmj@gmail.com>
2017-07-21 15:16:29 +0100

    updated socket routine for round trip data

--- toxicCheese/socket_client.py
+++ toxicCheese/socket_client.py
@@ -3,11 +3,14 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+ip_addr = raw_input('What IP ?: ')
+message = raw_input('Message to send: ')
 
-s.connect((host, port))
+port = 1776                # Reserve a port for your service.
+
+s.connect((ip_addr, port))
 print s.recv(1024)
+s.send(message)
 s.close                     # Close the socket when done
 
 
