47fd69482dc94e56989d46b4bae44f64c407518e
James Hui <huitmj@gmail.com>
2017-07-24 16:06:10 +0100

    cleaned up code

--- toxicCheese/SenseWater.py
+++ toxicCheese/SenseWater.py
@@ -7,56 +7,32 @@ import requests
 from sys import exit
 
 
+#globals
 
 url = "http://10.160.50.195/humidity.php"
 WaterFileLoc = "/home/pi/Desktop/WaterFlow.csv"
-
-
-now = datetime.datetime.now()
-GPIO.setwarnings(False)
-# tell the GPIO module that we want to use 
-# the chip's pin numbering scheme
-GPIO.setmode(GPIO.BCM)
-
-
 #relation ship between raw number and liters
 timesFactorForMin = 230
 timesFactorForSec = 13800
-
-
-HouseNumber = 21
-Senser = "Rate Of Liquid Flow"
-
 #Location of file storage, with name which is changing depending on date
-WaterDataLocation = "/home/pi/Desktop/HouseSensors/toxicCheese/House" + str(HouseNumber) + "/"+ str(HouseNumber) +"WaterData_" + (now.strftime("%Y_%m_%d")) + ".csv"
-WaterDataDirectory = "/home/pi/Desktop/HouseSensors/toxicCheese/House" + str(HouseNumber)
-
-#Creats a new file if one doesn't exsist
-if not os.path.exists(WaterDataDirectory):
-    os.makedirs(WaterDataDirectory)
-
-
+HouseNumber = 21
+WaterDataLocation = "/home/pi/Desktop/House" + str(HouseNumber) + "/"+ str(HouseNumber) +"WaterData_" + (now.strftime("%Y_%m_%d")) + ".csv"
+WaterDataDirectory = "/home/pi/Desktop/House" + str(HouseNumber)
+now = datetime.datetime.now()
 icounter = 0
 start = 0
-#end = 0
 
-sMessageWater = ""
+def init():
 
+    GPIO.setwarnings(False)
+    # tell the GPIO module that we want to use 
+    # the chip's pin numbering scheme
+    GPIO.setmode(GPIO.BCM)
 
-
-
- 
-# handle the button event
-def buttonEventHandler (pin):
-    global icounter
-    global start
-    if start == 0:
-        start = time.time()
-
-    icounter += 1
-
-
-def StartUp():
+    #Creats a new file if one doesn't exsist
+    if not os.path.exists(WaterDataDirectory):
+        os.makedirs(WaterDataDirectory)
+        
     #setup pin 23 as an input
     # and set up pins 24 and 25 as outputs
     GPIO.setup(23,GPIO.IN)
@@ -68,38 +44,36 @@ def StartUp():
     # the buttonEventHandler function
     GPIO.add_event_detect(23,GPIO.FALLING)
     GPIO.add_event_callback(23,buttonEventHandler) 
-    
 
+# handle the button event
+def buttonEventHandler (pin):
+    global icounter
+    global start
+    if start == 0:
+        start = time.time()
+
+    icounter += 1
 
 # main function
-def main():
+def getWaterFlow():
     global icounter
     global start
     
     while True:
-        #time.sleep(1)
         TimeDiff = time.time() - start
         if TimeDiff > 10:
             return 0
         if (TimeDiff < 1.01) and (TimeDiff > 0.99): #Needs to be range as time can never be exactly one
             now = datetime.datetime.now()
-            #timestanp
-            sWaterMessage = str(now.strftime("%H:%M:%S")) + "," + str(round(icounter/timesFactorForMin, 4)) + "\n"
+            #timestamp
+            water =  round(icounter/timesFactorForMin, 4)
             #save to file
-            Waterfile = open(WaterDataLocation,"a")
-            Waterfile.write(sWaterMessage)
-            Waterfile.close()
-            return (sWaterMessage)
-
-
-#loops to gather more readings
-StartUp()
-
-
+            return (water)
+init()
 while True:
     icounter = 0
     start = 0
-    data = {'waterflow': main(), 
+    data = {'waterflow': getWaterFlow(), 
             'date': str(now.strftime("%Y_%m_%d")), 
             'timestamp': str(now.strftime("%H:%M:%S"))
             }
@@ -127,3 +101,4 @@ while True:
         print("HTTP error, exitting")
         sys.exit(1)
     time.sleep(10)
+    
