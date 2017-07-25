b27e5184be36d720b93fd22c49ed86c51b983a92
vikke1234 <viktor.horsmanheimo@gmail.com>
2017-07-25 15:23:59 +0100

    cleaned repo

--- toxicCheese/post.py
+++ toxicCheese/post.py
@@ -1,16 +1,20 @@
+#! /usr/bin/env python
 import requests
-from sys import sys.exit
-from os import os.path.isfile
+import os
+import sys
 from sense_hat import SenseHat
 from datetime import datetime
+from time import sleep
 
 
 url = "https://10.160.50.195/humidity.php"
 humidityFileLoc = "/var/www/html/humidity.csv"
 tempFile = "/var/www/html/temp.csv"
 sense = SenseHat()
+
 while (1):
     data = {'humidity': sense.get_humidity(), 
+            'temperature': sense.get_temperature(),
             'date': str(datetime.now().strftime("%Y-%m-%d")), 
             'timestamp': str(datetime.now().strftime("%H:%M%S"))
             }
@@ -36,3 +40,5 @@ while (1):
     except requests.exceptions.HTTPError:
         print("HTTP error, exitting")
         sys.exit(1)
+    sleep(10)
+
