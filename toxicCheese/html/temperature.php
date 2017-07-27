f34a857fef97b8662c00a6fee188780c4b8c2ca3
Mark <m1pearson@me.com>
2017-07-27 12:15:39 +0100

    Made input temp vaules

---
+++ toxicCheese/html/temperature.php
@@ -0,0 +1,29 @@
+<?php
+$host = "127.0.0.1";
+$dbname = "sensor";
+$user = "root";
+$password = "";
+
+$pdo = new PDO("mysql:dbname=$dbname;host=$host" , $user , $password);
+
+// Get temperature POST variables 
+
+$temperatureValue = $_POST['temperature'];
+$timestamp = $_POST['date'] . " " . $_POST['timestamp'];
+$macaddress = $_POST['mac'];
+
+
+
+// Insert temperature POST values into database
+
+$stmt = $pdo->prepare("INSERT INTO temperature (temperature, timestamp, macaddress) values (:temperature, :time, :mac)");
+$stmt->bindParam(':temperature', $temperatureVal, PDO::PARAM_INT);
+$stmt->bindParam(':time', $timestamp, PDO::PARAM_STR);
+$stmt->bindParam(':mac', $macaddress, PDO::PARAM_STR);
+$result = $stmt->execute();
+echo "OK :";
+print_r ($_POST);
+
+
+
+?>
