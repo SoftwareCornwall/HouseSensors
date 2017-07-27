9e07a0c686cb3c9eadfb6456a62e27f9c7403be1
LukeBowden <lukebowden1@gmail.com>
2017-07-27 11:09:40 +0100

    file for returning water usage for the app(currently past hour only)

---
+++ toxicCheese/returnValues.php
@@ -0,0 +1,11 @@
+<?php
+	$DB = new database();
+
+	try {
+		$waterUsage = $DB->total_water_sensor_usage_last_hour();
+		echo $waterUsage;
+	}
+	catch {
+		echo "error calculting total water usage";
+	}
+?>
